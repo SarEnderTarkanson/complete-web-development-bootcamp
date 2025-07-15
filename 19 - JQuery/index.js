9bc8c8e547af07aa702b38855a628d0621da6893
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-07-16 01:26:44 +0200

    website animations with jQuery

--- 19 - JQuery/index.js
+++ 19 - JQuery/index.js
@@ -16,6 +16,34 @@
 //     $("h1").css("color", "purple")
 // })
 
-$("h1").before("<button>New</button>")
+// $("h1").before("<button>New</button>")
 
-$("button").remove()
+// $("button").remove()
+
+// $("button").click(function () {
+//   $("h1").hide();
+// });
+
+// $("button").click(function () {
+//   $("h1").show();
+// });
+
+// $("button").click(function () {
+//   $("h1").toggle();
+// });
+
+// $("button").on("click",function () {
+//   $("h1").fadeToggle();
+// });
+
+// $("button").on("click",function () {
+//   $("h1").slideToggle();
+// });
+
+// $("button").on("click",function () {
+//   $("h1").animate({opacity:0.5});
+// });
+
+$("button").on("click",function () {
+  $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
