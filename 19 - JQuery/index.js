8b596695bc1847de00ec91a8890c5882d98392c7
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-07-16 01:08:48 +0200

    adding event listeners with jQuery

--- 19 - JQuery/index.js
+++ 19 - JQuery/index.js
@@ -1,1 +1,17 @@
-$("a").attr("href", "https://www.yahoo.com");
+// for (var i = 0; i < 5, i++; ) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function () {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// $("button").click(function () {
+//   $("h1").css("color", "purple");
+// });
+
+// $(document).keydown(function (event) {
+//   $("h1").text(event.key);
+// });
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "purple")
+})
