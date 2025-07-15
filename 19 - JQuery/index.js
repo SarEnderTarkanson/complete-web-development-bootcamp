f02434988c87edfbb8c9101d865c1cc7119994e7
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-07-16 01:13:14 +0200

    adding and removing elements with jQuery

--- 19 - JQuery/index.js
+++ 19 - JQuery/index.js
@@ -12,6 +12,10 @@
 //   $("h1").text(event.key);
 // });
 
-$("h1").on("mouseover", function(){
-    $("h1").css("color", "purple")
-})
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color", "purple")
+// })
+
+$("h1").before("<button>New</button>")
+
+$("button").remove()
