8ea7efe0af50c68222ce69f0c53d9836b74a3f03
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-07-17 01:56:43 +0200

    created the flow to create a random game pattern

--- 20 - Boss Level Challenge 2 - The Simon Game/game.js
+++ 20 - Boss Level Challenge 2 - The Simon Game/game.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var randomChosenColour = buttonColors[nextSequence()];
+
+var gamePattern = []
+
+gamePattern.push(randomChosenColour)
+
+function nextSequence() {
+  var randomNumber = Math.random();
+  randomNumber = randomNumber * 3 + 1;
+  randomNumber = Math.floor(randomNumber);
+  return randomNumber;
+}
