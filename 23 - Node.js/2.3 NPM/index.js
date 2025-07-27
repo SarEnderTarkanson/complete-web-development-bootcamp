255643c3e01a739a6f94327fd50167810a8d78a4
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-07-27 03:04:25 +0200

    the npm package manager & installing external node modules

---
+++ 23 - Node.js/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from "sillyname";
+
+var sillyname = generateName();
+
+console.log(`My name is ${sillyname}`);
+
+import {randomSuperhero} from "superheroes";
+
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
